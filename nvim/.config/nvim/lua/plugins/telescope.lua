0943c2b8a6c81a38a6ae7e0ef7665048fc0e60a3
Alex Lehman <alexlehman99@proton.me>
2025-07-18 19:57:04 -0400

    Making telescope show hidden files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,8 +3,8 @@ return {
     version = "*",
     cmd = { "Telescope" }, -- Plugin loads only when you run :Telescope
     keys = {
-        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-        { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+        { "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>", desc = "Find Files" },
+        { "<leader>fg", "<cmd>Telescope live_grep hidden=true<cr>", desc = "Live Grep" },
         { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
         { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
     },
