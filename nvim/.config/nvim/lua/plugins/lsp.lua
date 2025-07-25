15f33f1db4254f65a5519ff0c979cdd6ac3f4dc7
Alex Lehman <alexlehman99@proton.me>
2025-07-24 22:10:22 -0400

    Simplifying neovim lsp config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,40 +1,25 @@
 return {
-    -- Mason: LSP Installer
     {
-        "williamboman/mason.nvim",
+        "mason-org/mason.nvim",
         version = "*",
-        config = function()
-            require("mason").setup()
-        end
+        opts = {},
     },
 
     -- Mason-LSPConfig: Ensures installed LSPs integrate with nvim-lspconfig
     {
-        "williamboman/mason-lspconfig.nvim",
+        "mason-org/mason-lspconfig.nvim",
         version = "*",
-        dependencies = { "williamboman/mason.nvim" },
+        opts = {},
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
     },
 
     -- LSPConfig: Configures LSPs for Neovim
     {
         "neovim/nvim-lspconfig",
         version = "*",
-        dependencies = { "williamboman/mason-lspconfig.nvim" },
-        config = function()
-            local lspconfig = require("lspconfig")
-
-            -- Setup LSPs
-            local servers = {
-                "clangd", "pyright", "rust_analyzer", "gopls", "ts_ls", "lua_ls", "jdtls",
-                "html", "cssls", "jsonls", "yamlls",
-                "dockerls", "bashls", "terraformls",
-                "intelephense", "kotlin_language_server", "solargraph"
-            }
-
-            for _, server in ipairs(servers) do
-                lspconfig[server].setup {}
-            end
-        end
     }
 }
 
