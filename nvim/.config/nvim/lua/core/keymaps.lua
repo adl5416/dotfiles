a919b54e25aa02f8b7e2556bba633a35dfed10ac
Alex Lehman <alexlehman99@proton.me>
2026-03-03 11:21:38 -0500

    Add gd keymap to LSP definition

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -3,5 +3,8 @@
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
+-- LSP
+-- Go to definition
+vim.keymap.set("n", "gd", ":lua vim.lsp.buf.definition()<CR>")
