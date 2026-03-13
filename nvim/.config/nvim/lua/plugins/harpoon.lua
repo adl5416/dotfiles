a03dcde51c3d537fa3666a7295401892f127178f
Alex Lehman <alexlehman99@proton.me>
2026-03-13 09:28:17 -0400

    Add more marks to harpoon

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -11,6 +11,10 @@ return {
     vim.keymap.set("n", "<leader>m2", function() harpoon:list():select(2) end, { desc = "Go to Harpoon File 2" })
     vim.keymap.set("n", "<leader>m3", function() harpoon:list():select(3) end, { desc = "Go to Harpoon File 3" })
     vim.keymap.set("n", "<leader>m4", function() harpoon:list():select(4) end, { desc = "Go to Harpoon File 4" })
+    vim.keymap.set("n", "<leader>m5", function() harpoon:list():select(5) end, { desc = "Go to Harpoon File 5" })
+    vim.keymap.set("n", "<leader>m6", function() harpoon:list():select(6) end, { desc = "Go to Harpoon File 6" })
+    vim.keymap.set("n", "<leader>m7", function() harpoon:list():select(7) end, { desc = "Go to Harpoon File 7" })
+    vim.keymap.set("n", "<leader>m8", function() harpoon:list():select(8) end, { desc = "Go to Harpoon File 8" })
     vim.keymap.set("n", "<leader>mr", function() harpoon:list():remove() end, { desc = "Remove File from Harpoon" })
   end
 }
