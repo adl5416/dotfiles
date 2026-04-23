b06da14a260e5db81b55b15f4c67f72ba64f096e
Alex Lehman <alexlehman99@proton.me>
2026-04-23 12:57:44 -0400

    Add keymap for Tab

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -5,6 +5,16 @@
 --  See `:help hlsearch`
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
+-- Tab / Shift-Tab to indent/dedent
+vim.keymap.set("n", "<Tab>", ">>", { desc = "Indent line" })
+vim.keymap.set("n", "<S-Tab>", "<<", { desc = "Dedent line" })
+-- Keep lines highlighted in visual mode after indenting
+vim.keymap.set("v", "<Tab>", ">gv", { desc = "Indent selection" })
+vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Dedent selection" })
+-- Remove previous keybinds for Tab
+vim.keymap.set("n", ">>", "<Nop>")
+vim.keymap.set("n", "<<", "<Nop>")
+
 -- LSP
 -- Go to definition
 vim.keymap.set("n", "gd", ":lua vim.lsp.buf.definition()<CR>")
