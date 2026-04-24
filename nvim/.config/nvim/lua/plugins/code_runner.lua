bcaa3a2f3fe8e225ee69c42c04681c2f2e371609
Alex Lehman <alexlehman99@proton.me>
2026-04-24 15:49:34 -0400

    Add code runner to nvim config

---
+++ nvim/.config/nvim/lua/plugins/code_runner.lua
@@ -0,0 +1,35 @@
+return {
+    "CRAG666/code_runner.nvim",
+    cmd = { "RunCode", "RunFile", "RunProject", "RunClose" },
+    keys = {
+        { "<leader>rr", "<cmd>RunCode<cr>", desc = "Run Code" },
+        { "<leader>rf", "<cmd>RunFile<cr>", desc = "Run File" },
+        { "<leader>rp", "<cmd>RunProject<cr>", desc = "Run Project" },
+        { "<leader>rc", "<cmd>RunClose<cr>", desc = "Run Close" },
+    },
+    opts = {
+        mode = "term",
+        focus = true,
+        startinsert = true,
+        filetype = {
+            python = "python3 -u",
+            typescript = "deno run",
+            javascript = "node",
+            lua = "lua",
+            go = "go run",
+            rust = {
+                "cd $dir &&",
+                "rustc $fileName &&",
+                "$dir/$fileNameWithoutExt",
+            },
+            c = "cd $dir && gcc $fileName -o /tmp/$fileNameWithoutExt && /tmp/$fileNameWithoutExt",
+            cpp = "cd $dir && g++ $fileName -o /tmp/$fileNameWithoutExt && /tmp/$fileNameWithoutExt",
+            java = {
+                "cd $dir &&",
+                "javac $fileName &&",
+                "java $fileNameWithoutExt",
+            },
+            sh = "bash",
+        },
+    },
+}
