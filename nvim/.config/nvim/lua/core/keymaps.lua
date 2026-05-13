756d28a453bac59f499e705f5566a60cf6c65e5d
Alex Lehman <alexlehman99@proton.me>
2026-05-13 15:11:31 -0400

    Update keymaps for quit

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -15,11 +15,11 @@ vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Dedent selection" })
 vim.keymap.set("n", ">>", "<Nop>")
 vim.keymap.set("n", "<<", "<Nop>")
 
--- Save / Quit (common GUI keybinds)
+-- Save / Quit
 vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<cmd>up<CR>", { desc = "Save" })
 vim.keymap.set({ "n", "i", "v" }, "<C-S-s>", "<cmd>wa<CR>", { desc = "Save all" })
-vim.keymap.set({ "n", "i", "v" }, "<C-c>", "<cmd>q<CR>", { desc = "Quit" })
-vim.keymap.set({ "n", "i", "v" }, "<C-M-c>", "<cmd>qa!<CR>", { desc = "Force quit all" })
+vim.keymap.set({ "n", "i", "v" }, "<C-q>", "<cmd>q<CR>", { desc = "Quit" })
+vim.keymap.set({ "n", "i", "v" }, "<C-S-q>", "<cmd>qa!<CR>", { desc = "Force quit all" })
 vim.keymap.set({ "n", "i", "v" }, "<C-w>", "<cmd>wqa<CR>", { desc = "Save all and quit" })
 
 -- LSP
